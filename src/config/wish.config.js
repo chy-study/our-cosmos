ed004e4e3799e9eec63076b5b72c2dbdd1867ddc
CHY <1919216404@qq.com>
2026-05-12 22:37:08 +0800

    1

--- src/config/wish.config.js
+++ src/config/wish.config.js
@@ -7,6 +7,6 @@ export default [
   { title: '一起跨年', done: true, detail: '2026年一起迎接新年' },
   { title: '一起做饭', done: false, detail: '学做对方最爱吃的菜' },
   { title: '一起拍情侣写真', done: false, detail: '记录最美的我们' },
-  { title: '一起坐摩天轮', done: false, detail: '在最高处亲吻' },
+  { title: '一起坐摩天轮', done: false, detail: '在最高处看风景' },
   { title: '手牵手走遍每个城市', done: false, detail: '用脚步丈量这个世界' }
 ]
