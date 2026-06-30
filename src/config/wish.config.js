cf247b48441c420abd5b24f844037420ed374eba
CHY <1919216404@qq.com>
2026-06-30 18:26:24 +0800

    1

--- src/config/wish.config.js
+++ src/config/wish.config.js
@@ -1,7 +1,9 @@
 export default [
-  { title: '一起去鼋头渚', done: true, detail: '吃素面' },
   { title: '一起去徐霞客故居', done: true, detail: '打卡旅行家' },
+  { title: '一起去鼋头渚', done: true, detail: '吃素面' },
   { title: '一起爬惠山', done: true, detail: '小小爱心线💗，轻松拿捏~' },
+  { title: '一起逛惠山古镇', done: true, detail: '端午节去的，人从众' },
+  { title: '一起徒步军嶂古道小环线', done: true, detail: '李子线，摘了好多李子~' },
   { title: '一起去看海', done: false, detail: '想去三亚或者厦门，看日出日落' },
   { title: '一起去东北', done: false, detail: '东北二人转' },
   { title: '一起跨年', done: true, detail: '2026年一起迎接新年' },
