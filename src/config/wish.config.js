fae97497e9a321a9f22f135aefc1777cc1c11be8
CHY <1919216404@qq.com>
2026-07-24 08:49:41 +0800

    1

--- src/config/wish.config.js
+++ src/config/wish.config.js
@@ -4,6 +4,7 @@ export default [
   { title: '一起爬惠山', done: true, detail: '小小爱心线💗，轻松拿捏~' },
   { title: '一起逛惠山古镇', done: true, detail: '端午节去的，人从众' },
   { title: '一起徒步军嶂古道小环线', done: true, detail: '李子线，摘了好多李子~' },
+  { title: '一起玩123木头人游戏', done: false, detail: '能拿奖吗？靠敏了~' },
   { title: '一起去看海', done: false, detail: '想去三亚或者厦门，看日出日落' },
   { title: '一起去东北', done: false, detail: '东北二人转' },
   { title: '一起跨年', done: false, detail: '2026年一起迎接新年' },
